6bd54c76c7f54c6d3fc8a7838d553998c772be73
Athan <jonpuray@gmail.com>
2025-10-26 21:51:31 +0800

    updated

--- common.py
+++ common.py
@@ -7,6 +7,7 @@ from io import BytesIO
 @st.cache_resource
 def get_client():
     try:
+        st.write(st.secrets['mongodb']['uri'])
         return MongoClient(st.secrets['mongodb']['uri'])
     except Exception as e:
         st.write(e)

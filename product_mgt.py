6afadbb3df3c2835b05ef5adf566f48f1dfa52c1
Athan <jonpuray@gmail.com>
2025-10-27 03:19:04 +0800

    updated

--- product_mgt.py
+++ product_mgt.py
@@ -36,8 +36,6 @@ def add_item():
 
     # --- Reset inputs before rendering if requested ---
     if "clear_inputs" in st.session_state and st.session_state.clear_inputs:
-        st.session_state['item_size'] = 1
-        st.session_state['unit_size'] = 'GRAMS'
         for key in ['item_name' 'item_manufacturer']:
             st.session_state[key] = ''
         st.session_state.clear_inputs = False
